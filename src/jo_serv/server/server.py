216f424bdca7a7c0d9928b2739b5f1b0ea559ac9
Pierrick Curt <pierrick.curt@rtone.fr>
2022-06-07 21:23:35 +0200

    feat(jo_serv) Add token endpoint

--- src/jo_serv/server/server.py
+++ src/jo_serv/server/server.py
@@ -2,7 +2,8 @@
 import hashlib
 import json
 import logging
-
+import os
+import re
 import mariadb  # type: ignore
 from flask import Flask, Response, request  # type: ignore
 
@@ -16,6 +17,7 @@ def create_server() -> Flask:
     logger = logging.getLogger(__name__)
     logger.info("Create the server")
     app = Flask(__name__)
+
     try:
         conn = mariadb.connect(user="root", database="JO")
         cur = conn.cursor()
@@ -88,5 +90,51 @@ def create_server() -> Flask:
                 return Response(response=file.read(), status=200)
 
         return Response(response="Error on endpoint teams", status=404)
+
+    @app.route("/pushtoken", methods=["POST"])
+    def pushtoken() -> Response:
+        decode_data = request.data.decode("utf-8")
+        json_data = json.loads(decode_data)
+        logger.info(f"Data received : {decode_data}")
+
+        if json_data.get("token"):
+            if not os.path.exists("tokens.txt"):
+                open("tokens.txt", "w")
+            if (
+                json_data.get("token") not in open(data_dir + "/tokens.txt", "r").read()
+            ):  # just to be sure we don't write again the same
+                if json_data.get("username"):
+                    open(data_dir + "/tokens.txt", "a").write(
+                        json_data.get("token") + ":" + json_data.get("username") + "\n"
+                    )
+                else:
+                    open(data_dir + "/tokens.txt", "a").write(json_data.get("token") + ":\n")
+            elif json_data.get("username") != "":
+                lines = open(data_dir +"/tokens.txt", "r").readlines()
+                for local_token in lines:
+                    if json_data.get("token") in local_token:
+                        if (
+                            json_data.get("username")
+                            != local_token.split(":")[-1].replace("\n", "")
+                            and json_data.get("username") != ""
+                        ):
+                            logger.info("This token changed login!")
+                            to_update = True
+                            break
+                        else:
+                            to_update = False
+                            logger.info("I know already this token")
+                if to_update:
+                    raw_txt = open(data_dir + "/tokens.txt", "r").read()
+                    logger.info("(" + json_data.get("token") + ".*)")
+                    regexp = re.findall(
+                        "(" + re.escape(json_data.get("token")) + ".*)", raw_txt
+                    )[0]
+                    raw_txt = raw_txt.replace(
+                        regexp,
+                        json_data.get("token") + ":" + json_data.get("username") + "\n",
+                    )
+                    open(data_dir + "/tokens.txt", "w").write(raw_txt)
+        return Response(response="fdp", status=200)
 
     return app
